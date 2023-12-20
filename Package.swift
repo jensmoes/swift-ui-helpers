e6ff7b07b816fcfa57541c318f7888d0ee9a856b
jensmoes <jens@troest.com>
2023-12-20 11:12:01 -0800

    Add documentation

--- Package.swift
+++ Package.swift
@@ -15,6 +15,9 @@ let package = Package(
             name: "swift-ui-helpers",
             targets: ["SwiftUIHelpers"]),
     ],
+    dependencies: [
+            .package(url: "git@github.com:apple/swift-docc-plugin.git", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
